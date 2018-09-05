c0d2139589517406a2a241a023babfccab65a83f
Jethro W. Fredericks <fredericks22.jethro@gmail.com>
2018-09-05 17:33:12 +0200

    Test POST /auth/login route

--- server/tests/auth.test.js
+++ server/tests/auth.test.js
@@ -6,9 +6,14 @@ const User = require('../models/user');
 const { populateUsers, users } = require('./seed/seed');
 
 describe('/auth', () => {
-  describe('/register', () => {
-    let user;
+  let user;
 
+  before(function(done) {
+    this.timeout(0);
+    populateUsers(done);
+  });
+
+  describe('/register', () => {
     before(function(done) {
       this.timeout(0);
       populateUsers(done);
@@ -67,6 +72,55 @@ describe('/auth', () => {
         .expect(400)
         .expect(res => {
           expect(res.body[0]).to.equal('This email is already in use');
+        })
+        .end(done);
+    });
+  });
+
+  describe('/login', () => {
+    it('should return user details and token on successful login', done => {
+      user = users[0];
+
+      request(app)
+        .post('/auth/login')
+        .send(user)
+        .expect(200)
+        .expect(res => {
+          expect(res.headers['x-auth']).to.be.a('string');
+          expect(res.body.username).to.equal(user.username);
+          expect(res.body.handle).to.equal(user.handle);
+        })
+        .end(done);
+    });
+
+    it('should return an error message if password does not match', done => {
+      user = {
+        email: users[0].email,
+        password: 'notmyrealpassword'
+      };
+
+      request(app)
+        .post('/auth/login')
+        .send(user)
+        .expect(400)
+        .expect(res => {
+          expect(res.body.msg).to.equal('Password does not match');
+        })
+        .end(done);
+    });
+
+    it('should return an error message if the user does not exist', done => {
+      user = {
+        email: 'TinyTina@gmail.com',
+        password: 'iamtinytina'
+      };
+
+      request(app)
+        .post('/auth/login')
+        .send(user)
+        .expect(400)
+        .expect(res => {
+          expect(res.body.msg).to.equal('User not found');
         })
         .end(done);
     });

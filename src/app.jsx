679d355e05960e4c87a9737e511b81d42e0c3795
Jethro W. Fredericks <fredericks22.jethro@gmail.com>
2018-09-05 23:30:06 +0200

    Connect app to redux store

--- src/app.jsx
+++ src/app.jsx
@@ -1,8 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
 
+import configureStore from './store/configureStore';
 import LandingPage from './components/LandingPage';
 
 const appRoot = document.getElementById('app');
 
-ReactDOM.render(<LandingPage />, appRoot);
+const store = configureStore();
+
+const root = (
+  <Provider store={store}>
+    <LandingPage />
+  </Provider>
+);
+
+ReactDOM.render(root, appRoot);

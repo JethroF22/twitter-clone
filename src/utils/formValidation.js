6f7a5235805d0c2d3789b7d77d9137d2665c0bf8
Jethro W. Fredericks <fredericks22.jethro@gmail.com>
2018-09-06 22:06:41 +0200

    Fix bugs in `validateRegistrationForm()`

--- src/utils/formValidation.js
+++ src/utils/formValidation.js
@@ -5,29 +5,31 @@ export const validateRegistrationForm = formData => {
   for (const key in formData) {
     switch (key) {
       case 'password':
-        if (data[key].length < 8) {
+        if (formData[key].length < 8) {
           errors[key] = 'Password must be at least 8 characters';
         }
         break;
       case 'confirmPassword':
-        if (data[key].length !== data['password']) {
+        if (formData[key] !== formData['password']) {
           errors[key] = 'Passwords must match';
         }
         break;
       case 'username':
       case 'handle':
-        if (data[key].length < 6) {
+        if (formData[key].length < 6) {
           errors[key] = `${key.charAt(0).toUpperCase() +
             key.slice(1)} must be at least 6 characters`;
         }
         break;
       case 'email':
-        if (!validator.isEmail(data[key])) {
-          errors[key] = `"${data[key]}" is not a valid email`;
+        if (!validator.isEmail(formData[key])) {
+          errors[key] = `"${formData[key]}" is not a valid email`;
         }
         break;
       default:
         break;
     }
   }
+
+  return errors;
 };

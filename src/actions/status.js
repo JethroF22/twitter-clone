0493106dfe67c10a66a391a4cb9eadde1e8986a5
Jethro W. Fredericks <fredericks22.jethro@gmail.com>
2018-09-08 00:03:28 +0200

    Fix error in `src/actions/status.js`

--- src/actions/status.js
+++ src/actions/status.js
@@ -1,6 +1,6 @@
 import { actionTypes } from '../config/const.json';
 
-const { SET_ACTION_STATUS } = actionTypes.error;
+const { SET_ACTION_STATUS } = actionTypes.status;
 
 export const setActionStatus = ({ status, actionName }) => ({
   type: SET_ACTION_STATUS,

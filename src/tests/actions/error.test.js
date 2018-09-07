1e69907c2f27ea0de94ecb491a488d143edca8c5
Jethro W. Fredericks <fredericks22.jethro@gmail.com>
2018-09-07 23:32:59 +0200

    Test `setError` action

---
+++ src/tests/actions/error.test.js
@@ -0,0 +1,20 @@
+import { setError } from '../../actions/error';
+import {
+  actionTypes,
+  errorTypes,
+  errorMessages
+} from '../../config/const.json';
+
+const { SET_ERROR_MESSAGE } = actionTypes.error;
+const { UNKNOWN_ERROR } = errorMessages;
+const { DB_ERROR } = errorTypes;
+
+let actionObject = {
+  errorMessage: UNKNOWN_ERROR,
+  errorType: DB_ERROR
+};
+
+test('should create the action object', () => {
+  const action = setError(actionObject);
+  expect(action).toEqual({ type: SET_ERROR_MESSAGE, ...actionObject });
+});

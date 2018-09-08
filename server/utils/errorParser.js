fcff763df2909cbd15c65db33fb027a5f2e4c03a
Jethro W. Fredericks <fredericks22.jethro@gmail.com>
2018-09-08 22:38:30 +0200

    Refactor `DBErrorParser`

--- server/utils/errorParser.js
+++ server/utils/errorParser.js
@@ -1,10 +1,12 @@
 const DBErrorParser = err => {
   const errors = [];
   if (err.name === 'MongoError') {
-    errors.push('This email is already in use');
+    errors.push({
+      msg: `Duplicate ${err.errmsg.indexOf('email') > -1 ? 'email' : 'handle'}`
+    });
   } else {
     for (const error in err.errors) {
-      errors.push(err.errors[error].message);
+      errors.push({ msg: err.errors[error].message });
     }
   }
 

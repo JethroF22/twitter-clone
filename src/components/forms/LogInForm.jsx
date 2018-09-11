663bee4aadc7a23328fcd58653543c75337d4997
Jethro W. Fredericks <fredericks22.jethro@gmail.com>
2018-09-11 20:42:47 +0200

    Create `onSubmit` handler, connect `LogInForm` to store

--- src/components/forms/LogInForm.jsx
+++ src/components/forms/LogInForm.jsx
@@ -1,10 +1,19 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import _ from 'lodash';
+import validator from 'validator';
 
 import { logIn } from '../../actions/auth';
+import { actionStatusMessages, errorMessages } from '../../config/const.json';
 
-class LogInForm extends Component {
+const {
+  SUCCESS_MESSAGE,
+  FAILED_MESSAGE,
+  IN_PROGRESS_MESSAGE
+} = actionStatusMessages;
+const { INVALID_CREDENTIALS } = errorMessages;
+
+export class LogInForm extends Component {
   state = {
     email: '',
     password: '',
@@ -14,18 +23,46 @@ class LogInForm extends Component {
   onChange = e => {
     const { value, name } = e.target;
     this.setState(() => ({
-      [name]: value
+      [name]: value,
+      errors: {}
     }));
+  };
+
+  onSubmit = e => {
+    e.preventDefault();
+
+    const credentials = _.pick(this.state, ['email', 'password']);
+
+    if (validator.isEmail(credentials.email)) {
+      this.props.logIn(credentials).then(() => {
+        if (this.props.actionStatus === SUCCESS_MESSAGE) {
+          alert('successful');
+        } else if (this.props.actionStatus === FAILED_MESSAGE) {
+          this.setState(() => ({
+            errors: {
+              authenticationError: INVALID_CREDENTIALS
+            }
+          }));
+        }
+      });
+    } else {
+      this.setState(() => ({
+        errors: {
+          email: 'Invalid email address'
+        }
+      }));
+    }
   };
 
   render() {
     return (
       <div>
         <h3>Log In</h3>
-        <form>
+        <form onSubmit={this.onSubmit}>
           {this.state.errors.authenticationError && (
             <p>{this.state.errors.authenticationError}</p>
           )}
+          {this.state.errors.email && <p>{this.state.errors.email}</p>}
           <label htmlFor="email">Email: </label>
           <input
             type="email"
@@ -42,11 +79,29 @@ class LogInForm extends Component {
             onChange={this.onChange}
           />
           <br />
-          <button type="submit">Log In</button>
+          <button
+            type="submit"
+            disabled={this.props.actionStatus === IN_PROGRESS_MESSAGE}
+          >
+            Log In
+          </button>
         </form>
       </div>
     );
   }
 }
 
-export default LogInForm;
+const mapStateToProps = state => ({
+  actionStatus: state.status.actionStatus,
+  errorType: state.error.errorType,
+  errorMessage: state.error.errorMessage
+});
+
+const mapDispatchToProps = dispatch => ({
+  logIn: credentials => dispatch(logIn(credentials))
+});
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(LogInForm);
